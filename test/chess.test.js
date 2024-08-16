2dbfbdcc24bdc196b923aff64971e1525fed0685
Russell Wallace <russell.wallace@gmail.com>
2024-08-16 12:43:36 +0100

    board

--- test/chess.test.js
+++ test/chess.test.js
@@ -4,5 +4,11 @@ test('pieces are distinct', () => {
   expect(BISHOP).not.toBe(KNIGHT);
 });
 test('board size', () => {
-  expect(initialBoard(6,6)).length.toBe(36);
+  expect(initialBoard(6, 6).length).toBe(36);
+});
+test('minimum width', () => {
+  expect(() => initialBoard(0, 6)).toThrow();
+});
+test('minimum height', () => {
+  expect(() => initialBoard(6, 2)).toThrow();
 });
