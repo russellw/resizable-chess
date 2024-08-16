1bc5d73b3fed4835a8031c940083027ca775701b
Russell Wallace <russell.wallace@gmail.com>
2024-08-16 12:10:22 +0100

    board size

--- test/chess.test.js
+++ test/chess.test.js
@@ -3,3 +3,6 @@ Object.assign(global, require('../lib/chess'));
 test('pieces are distinct', () => {
   expect(BISHOP).not.toBe(KNIGHT);
 });
+test('board size', () => {
+  expect(initialBoard(6,6)).length.toBe(36);
+});
