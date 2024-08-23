1dd7154d74ba644d3c41584cd03751b9512882a3
Russell Wallace <russell.wallace@gmail.com>
2024-08-23 21:03:11 +0100

    capitalize-comments

--- fmt.bat
+++ fmt.bat
@@ -1,6 +1,7 @@
-call prettier --no-semi --print-width 132 -w .
-
 comment-space -w batch.js lib test
+capitalize-comments -w batch.js lib test
+
 js-equals -w batch.js lib test
 
+call prettier --no-semi --print-width 132 -w .
 git diff
