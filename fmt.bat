c598090b63a587fb908af720b61f88646d3545d6
Russell Wallace <russell.wallace@gmail.com>
2024-09-09 12:57:45 +0100

    do-all-recur

--- fmt.bat
+++ fmt.bat
@@ -1,6 +1,10 @@
-do-all-recur-js . comment-space -w 
-do-all-recur-js . capitalize-comments -w 
-do-all-recur-js . js-equals -w 
-
 call prettier --no-semi --print-width 132 -w .
+
+do-all-recur . comment-space -w
+do-all-recur . capitalize-comments -w
+
+do-all-recur . js-equals -w
+
+do-all-recur . remove-trail-space -w
+
 git diff
