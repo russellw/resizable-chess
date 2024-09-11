627b33b3559d156c4986d7e7dc29c39df8852477
Russell Wallace <russell.wallace@gmail.com>
2024-09-11 15:52:01 +0100

    fmt

--- fmt.bat
+++ fmt.bat
@@ -1,10 +1,12 @@
-call prettier --no-semi --print-width 132 -w .
+rem This should go first, because some of the others depend on it
+call prettier --no-semi --print-width 132 -w .||exit /b
 
-do-all-recur . comment-space -w
-do-all-recur . capitalize-comments -w
+rem These need to go in the specified order
+do-all-recur . comment-space -w||exit /b
+do-all-recur . capitalize-comments -w||exit /b
 
-do-all-recur . js-equals -w
-
-do-all-recur . remove-trail-space -w
+rem These can go in any order
+do-all-recur . js-equals -w||exit /b
+do-all-recur . remove-trail-space -w||exit /b
 
 git diff
