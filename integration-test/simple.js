9582808e7be442c8ffc21137a0b2c97914cb1fd1
Russell Wallace <russell.wallace@gmail.com>
2024-09-15 11:26:07 +0100

    fix

--- integration-test/simple.js
+++ integration-test/simple.js
@@ -2,7 +2,9 @@
 const assert = require("assert")
 Object.assign(global, require("../lib"))
 
-let board = initialBoard(1, 5)
+let board = emptyBoard(1, 5)
+board.put(0, 4, -KING)
+board.put(0, 0, KING)
 for (let depth = 1; depth <= 10; depth++) {
   const start = Date.now()
   const v = movesVals(board, depth)
