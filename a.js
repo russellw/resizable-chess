b85408d44fb528ba82a377e67038cc0f0354a425
Russell Wallace <russell.wallace@gmail.com>
2024-09-21 15:18:44 +0100

    chalk

--- a.js
+++ a.js
@@ -1,8 +1,12 @@
 "use strict"
+const chalk = require('chalk');
 Object.assign(global, require("./lib"))
 
-tron()
+// Print text with different colors
+console.log(chalk.blue('This text is blue!'));
+console.log(chalk.red('This text is red!'));
+console.log(chalk.green('This text is green!'));
 
-let a = initialBoard(4, 4)
-let b = bestMove(a, 1)
-dbg(getNodes())
+// You can also combine styles
+console.log(chalk.bold.yellow('This text is bold and yellow!'));
+console.log(chalk.bgCyan.black('This text has a cyan background and black text!'));
