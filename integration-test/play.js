b8829f2dad32fa5e2a17e29a04587a7477a3e509
Russell Wallace <russell.wallace@gmail.com>
2024-09-21 11:50:18 +0100

    old vs new versions

--- integration-test/play.js
+++ integration-test/play.js
@@ -1,24 +1,31 @@
 "use strict"
 const assert = require("assert")
-const oldEngine = require("index0")
+const old = require("./old")
 Object.assign(global, require("../lib"))
 
 let a = initialBoard(6, 6)
-for (let i = 1; ; i++) {
-  printBoard(a)
-  const start = Date.now()
-  const b = bestMove(a, 9)
-  if (b === null) {
-    const color = a.turn < 0 ? "white" : "black"
-    console.log(`${color} wins`)
-    break
-  }
-  a = b
-  const t = (Date.now() - start) / 1000
+
+function playMove(name, bestMove) {
+  console.log(name)
+  let start = Date.now()
+  a = bestMove(a, 8)
+  let t = (Date.now() - start) / 1000
   const n = getNodes()
-  console.log(i)
   console.log(n)
   console.log(t)
   console.log(n / t)
+  if (a === null) {
+    console.log("loses")
+    process.exit(0)
+  }
+  printBoard(a)
   console.log(a.val)
+  console.log()
+}
+
+for (let i = 1; ; i++) {
+  console.log(i)
+  console.log()
+  playMove("new (white)", bestMove)
+  playMove("old (black)", old.bestMove)
 }
