f24b7babc1b0ab8a8deb652764ffd6822a252e3a
Russell Wallace <russell.wallace@gmail.com>
2024-09-21 15:29:00 +0100

    chalk

--- a.js
+++ a.js
@@ -1,12 +1,11 @@
 "use strict"
-const chalk = require('chalk');
-Object.assign(global, require("./lib"))
+import chalk from "chalk"
 
-// Print text with different colors
-console.log(chalk.blue('This text is blue!'));
-console.log(chalk.red('This text is red!'));
-console.log(chalk.green('This text is green!'));
+// print text with different colors
+console.log(chalk.blue("This text is blue!"))
+console.log(chalk.red("This text is red!"))
+console.log(chalk.green("This text is green!"))
 
-// You can also combine styles
-console.log(chalk.bold.yellow('This text is bold and yellow!'));
-console.log(chalk.bgCyan.black('This text has a cyan background and black text!'));
+// you can also combine styles
+console.log(chalk.bold.yellow("This text is bold and yellow!"))
+console.log(chalk.bgCyan.black("This text has a cyan background and black text!"))
