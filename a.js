c7b16c620f80694deee47c112cf820a658b761d0
Russell Wallace <russell.wallace@gmail.com>
2024-09-23 14:05:07 +0100

    refactor

--- a.js
+++ a.js
@@ -1,27 +1,25 @@
 "use strict"
 import chalk from "chalk"
 import * as engine from "./lib/engine.js"
+import * as old from "./lib/engine.js"
 
 engine.initBoard()
-console.log(board)
-let white = new Engine(board, 1, "white")
-let black = new Engine(board, -1, "black")
+console.log(engine.board)
 
-function takeTurn(player, opponent) {
-  console.log(player.name)
+function takeTurn(name, makeMove, notifyMove, board) {
+  console.log(name)
   let start = Date.now()
-  let move = player.makeMove()
+  let move = makeMove()
   let t = (Date.now() - start) / 1000
   console.log(t)
   if (move === null) return false
-  putMove(move, board)
+  notifyMove(move)
   printBoard(move, board)
-  opponent.notifyMove(move)
   return true
 }
 
 for (let i = 1; ; i++) {
   console.log(i)
-  if (!takeTurn(white, black)) break
-  if (!takeTurn(black, white)) break
+  if (!takeTurn("white", engine.makeMove, old.notifyMove, engine.board)) break
+  if (!takeTurn("black", old.makeMove, engine.notifyMove, engine.board)) break
 }
