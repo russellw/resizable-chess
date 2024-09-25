2ca12640a130b48808e9e345f6c90066eaba53cf
Russell Wallace <russell.wallace@gmail.com>
2024-09-25 02:00:50 +0100

    1x1 board is a loss for white

--- test/engine.test.js
+++ test/engine.test.js
@@ -2,16 +2,29 @@
 import assert from "assert"
 // SORT
 import * as engine from "../engine.js"
-import { initialBoard } from "../rules.js"
+import { BISHOP, KING, KNIGHT, PAWN, QUEEN, ROOK, initialBoard } from "../rules.js"
 //
 
 describe("engine", function () {
-  it("null board is a loss for white", function () {
+  it("0x0 board is a loss for white", function () {
     const width = 0
     const height = 0
     const board = initialBoard(width, height)
     engine.init(width, height, board, 1)
     const move = engine.makeMove()
     assert.strictEqual(move, null)
+  })
+
+  it("1x1 board is a loss for white", function () {
+    const width = 1
+    const height = 1
+
+    let board = new Int8Array([0])
+    engine.init(width, height, board, 1)
+    assert.strictEqual(engine.makeMove(), null)
+
+    board = new Int8Array([KING])
+    engine.init(width, height, board, 1)
+    assert.strictEqual(engine.makeMove(), null)
   })
 })
